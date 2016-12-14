e5bbb7b4a490ab69f34123e58dd035f0b0290e7b
DXkite <DXkite@163.com>
2016-12-14 17:11:30 +0800

    each测试

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,18 @@
 module.exports = function (grunt) {
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            dxproject: {
-                src: ['src/dxtpl.js', 'src/*.js'],
-                dest: 'build/<%= pkg.name %>.js'
-            },
-        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> by <%= pkg.author %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'build/<%= pkg.name %>.js',
+                src: 'src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             },
         },
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['uglify']);
 
 };
